212de0b9955e95afbc268fd14460aeaa6b307ca1
PandaLinds <44917595+PandaLinds@users.noreply.github.com>
2019-04-01 14:04:17 -0700

    Adding length checking and error handlers
    I added a way to check the difference of 2 GPS coordinates in meters. I also added asserts as well. Need to add error handlers for ^c.

--- localization.cpp
+++ localization.cpp
@@ -1,3 +1,4 @@
+
 //Google C++ style guide is used
 
 // This code watches input from a GPS, using a wrapper of libgps
@@ -11,7 +12,7 @@
 
 // process ok? how to start?
 
-// g++ -Wall -pedantic $(pkg-config --cflags --libs libgps) localization.cpp -o localize
+// g++ -Wall $(pkg-config --cflags --libs libgps) localization.cpp -o localize
 //   We are allowing C++14 because that is the R-Pi's standard
 
 // functions other than main will be converted to .h files 
@@ -26,7 +27,8 @@ FILE *logfile_ptr;
 #include <sstream> //provides string stream classes
 #include <unistd.h> //for syslog
 #include <syslog.h> //for syslog
-//may need math.h for polar conversion
+#include <assert.h> //for asserts
+#include <cmath>  //for converting to polar coordinates
 
 #include <libgpsmm.h> // for GPS
 
@@ -35,13 +37,15 @@ using namespace std;
 //add class to .h file when created to match style guide.
 class LOCATION
 {
-  private:
+  //unblock after comparason working
+  //private:
+  public:
   //the lat/long data is in DBUS_TYPE_DOUBLE but will not compile.
   // will convert when necessary.
   double latitude, longitude;
   string timeStamp;
   
-  public:
+  //public:
   //funcitons LOCATION
   int saveGPSData(double, double, string);
   
@@ -62,64 +66,78 @@ int LOCATION::saveGPSData(double GPSlat, double GPSlong, string time)
   
 }
 
+
+double gpsComp(double lat1, double lat2, double long1, double long2)
+{
+  //compare lat/longs instead of polar
+  double hervsine, useHervsine, distanceMeters, earthRadMeters = 6371;
+  
+  hervsine =  (pow((sin((lat2-lat1)/2)),2)+(cos(lat1)*cos(lat2)*pow((sin((long2-long1)/2)),2)));
+  
+  useHervsine = 2*atan2(sqrt(hervsine), sqrt(1.0-hervsine));
+  
+  distanceMeters = earthRadMeters * useHervsine;
+  return distanceMeters;
+  
+}
+
+//split initialization function?
 void GPS() // this function will be added to .h file when it is created. 
 {
   //TODO:
-  //Convert to polar and see if 9 meters off,
+  //Convert to polar and see if 3 meters off,
   // then save new data
   
   LOCATION instance1;
   
   gpsmm gps_rec("localhost", DEFAULT_GPSD_PORT);   //finds GPS, see what # the default port is
   
-  if (gps_rec.stream(WATCH_ENABLE | WATCH_JSON) == NULL) // ENABLE turns off repording modes, JSON turns on JSON reporting data
-  {
-    std::cerr << "No GPSD running.\n";
-    //add error handler, possibly an assert
-  }
+  assert(gps_rec.stream(WATCH_ENABLE | WATCH_JSON) != NULL);
+  
+  //if (gps_rec.stream(WATCH_ENABLE | WATCH_JSON) == NULL) // ENABLE turns off repording modes, JSON turns on JSON reporting data
+  //{
+    //std::cerr << "No GPSD running.\n";
+    
+  //}
 
-  for (int idx = 0; idx < 10; idx++) 
-  {
-    if (!gps_rec.waiting(1000000)) continue;
-
-    struct gps_data_t *gpsd_data;  //style guide says put outside of forloop, will do at later date
-
-    if ((gpsd_data = gps_rec.read()) == NULL) 
-    {
-      std::cerr << "GPSD read error.\n";
-      //add error handler, possibly an assert
-    } 
-    struct gps_data_t gpsd_data;  //style guide says put outside of forloop, cannot test currently
+  struct gps_data_t gpsd_data;  
   struct gps_data_t *dataPtr = &gpsd_data;
+  double comp;
   
   for (;;)  // change back while forever
   {
-    if (!gps_rec.waiting(1000000)) continue;  //find what 1000000 is and make not magic number
+    //if (!gps_rec.waiting(1000000)) continue;  //find what 1000000 is and make not magic number
     
-
+    assert(gps_rec.waiting(1000000) == true );
     
-
-    if ((dataPtr = gps_rec.read()) == NULL) 
-    {
-      std::cerr << "GPSD read error.\n";
-    } 
-    else 
-    {
+    assert((dataPtr = gps_rec.read()) != NULL);
+    
+    //if ((dataPtr = gps_rec.read()) == NULL) 
+    //{
+      //std::cerr << "GPSD read error.\n";
+    //} 
+    //else 
+    //{
+    
+      //while loop is needed
       while (((dataPtr= gps_rec.read()) == NULL) ||
              (dataPtr->fix.mode < MODE_2D)) 
       {
 		// uncomment break to continue with fake GSP data
-		// break;
+		 break;
         // Do nothing until fix
+        cout<<"Stuck trying to fix"<<endl;
       }
-      
+      assert(dataPtr != NULL);
+      assert(dataPtr->fix.mode >= MODE_2D);
       // log the gps binary data
-      fwrite(dataPtr, sizeof(struct gps_data_t), 1, logfile_ptr);
+      assert((fwrite(dataPtr, sizeof(struct gps_data_t), 1, logfile_ptr)) == true);
+      
+      
        
       timestamp_t ts { dataPtr->fix.time };
-      auto latitude2  { dataPtr->fix.latitude };
-      auto longitude2 { dataPtr->fix.longitude };
-      cout<<longitude2<<endl;
+      auto latitude  { dataPtr->fix.latitude };
+      auto longitude { dataPtr->fix.longitude };
       
       // convert GPSD's timestamp_t into time_t
       time_t seconds { (time_t)ts };
@@ -128,10 +146,12 @@ void GPS() // this function will be added to .h file when it is created.
       std::ostringstream oss;
       oss << std::put_time(&tm, "%d-%m-%Y %H:%M:%S");
       auto time_str { oss.str() };
-      instance1.saveGPSData((double)latitude2, (double)longitude2, time_str);
-
+      instance1.saveGPSData((double)(latitude+0.000001), (double)(longitude+0.000001), time_str); //fix after comp works
       
-    }
+      comp = gpsComp(instance1.latitude, latitude, instance1.longitude, longitude);
+      cout<<"Distance: "<<comp<<endl;
+      
+    //}
   }
 }
 
@@ -141,14 +161,14 @@ void GPS() // this function will be added to .h file when it is created.
 int main(void)
 {
   // error check this - TBD
-  logfile_ptr = fopen("/tmp/gpslog.bin", "w");
+  assert ((logfile_ptr = fopen("/tmp/gpslog.bin", "w")) >= 0);
   
   GPS();
   cout<<"I made it this far"<<endl;
   
-  fclose(logfile_ptr);
+  assert((fclose(logfile_ptr)) == true);
 }
 
 
-//add int handler for exiting loop
+/*add int handler for exiting loop */
 // add asserts for return codes
