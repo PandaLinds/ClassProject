bdf2971dabccda276ea3e538090a7ddf03b4150b
Alec <aleclee1996@gmail.com>
2019-04-15 14:41:56 -0700

    Added signal handler, need to update the test

--- localization.cpp
+++ localization.cpp
@@ -21,6 +21,7 @@ FILE *logfile_ptr;
 #include <syslog.h> //for syslog
 #include <assert.h> //for asserts
 #include <cmath>  //for finding distances between coordinates
+#include <csignal>  // for ^c handler
 
 #include <libgpsmm.h> // for GPS
 
@@ -29,17 +30,16 @@ using namespace std;
 //add class to .h file when created to match style guide.
 class LOCATION
 {
-  //unblock after comparason working
-  //private:
-  public:
+  private:
   //the lat/long data is in DBUS_TYPE_DOUBLE but will not compile.
   // will convert when necessary.
   double latitude, longitude;
   string timeStamp;
   
-  //public:
+  public:
   //funcitons LOCATION
   int saveGPSData(double, double, string);
+  double gpsComp(double,double);
   LOCATION()
   {
     latitude = 0.0;
@@ -70,12 +70,12 @@ int LOCATION::saveGPSData(double GPSlat, double GPSlong, string time)
 }
 
 
-double gpsComp(double lat1, double lat2, double long1, double long2)
+double LOCATION::gpsComp(double lat, double lon)
 {
-  //compare lat/longs instead of polar
+  //compare lat/longs in meters using the Hervsine method
   double hervsine, useHervsine, distanceMeters, earthRadMeters = 6371;
   
-  hervsine =  (pow((sin((lat2-lat1)/2)),2)+(cos(lat1)*cos(lat2)*pow((sin((long2-long1)/2)),2)));
+  hervsine =  (pow((sin((latitude-lat)/2)),2)+(cos(lat)*cos(latitude)*pow((sin((longitude-lon)/2)),2)));
   
   useHervsine = 2*atan2(sqrt(hervsine), sqrt(1.0-hervsine));
   
@@ -84,89 +84,22 @@ double gpsComp(double lat1, double lat2, double long1, double long2)
   
 }
 
-#define SPOOF (1)
 
-//split initialization function?
-void GPS() // this function will be added to .h file when it is created. 
-{
-  //TODO:
-  //Convert to polar and see if 3 meters off,
-  // then save new data
-  
-  LOCATION instance1;
-  
-  gpsmm gps_rec("localhost", DEFAULT_GPSD_PORT);   //finds GPS, see what # the default port is
-  
-  assert(gps_rec.stream(WATCH_ENABLE | WATCH_JSON) != NULL);
-  
-  //if (gps_rec.stream(WATCH_ENABLE | WATCH_JSON) == NULL) // ENABLE turns off repording modes, JSON turns on JSON reporting data
-  //{
-    //std::cerr << "No GPSD running.\n";
-    
-  //}
+//Functions used in this program
+// split initialization function?
+void GPS(); // this function will be added to .h file when it is created. 
 
-  struct gps_data_t gpsd_data;  
-  struct gps_data_t *dataPtr = &gpsd_data;
-  double comp;
-  
-  for (;;)  // change back while forever
-  {
-    
-    assert(gps_rec.waiting(1000000) == true );
-    
-    assert((dataPtr = gps_rec.read()) != NULL);
-    
-    while (((dataPtr= gps_rec.read()) == NULL) ||
-             (dataPtr->fix.mode < MODE_2D)) 
-    {
-
-        // Do nothing until fix
-        cout<<"Stuck trying to fix"<<endl;
-      }
-
-      assert(dataPtr != NULL);
-      assert(dataPtr->fix.mode >= MODE_2D);
-      // log the gps binary data
-      assert((fwrite(dataPtr, sizeof(struct gps_data_t), 1, logfile_ptr)) == true);
-      
-      
-       
-      cout << "Time, lat, lon parsing" << endl;
-
-      timestamp_t ts { dataPtr->fix.time };
-      auto newLatitude  { dataPtr->fix.latitude };
-      auto newLongitude { dataPtr->fix.longitude };
-
-      cout << "Time, lat, lon parsed" << endl;
-      
-      // convert GPSD's timestamp_t into time_t
-      time_t seconds { (time_t)ts };
-      auto   tm = *std::localtime(&seconds);
-
-      cout << "Time convert" << endl;
-
-      std::ostringstream oss;
-      oss << std::put_time(&tm, "%d-%m-%Y %H:%M:%S");
-      auto time_str { oss.str() };
-      
-      cout << "Calling GPS comp" << endl;
-      comp = gpsComp(instance1.latitude, newLatitude, instance1.longitude, newLongitude);
-      cout<<"Distance: "<<comp<<endl;
-      if (comp >= 3.0) 
-      {     
-        instance1.saveGPSData((double)(newLatitude), (double)(newLongitude), time_str); //fix after comp works
-      }
-      
-      
-    //}
-  }
-}
+void signalHandler(int);
 
 
+//global so signal_handler will work
+LOCATION instance1;
 
 
 int main(void)
 {
+  //registering signal SIGINT and signal handler 
+  signal(SIGINT, signalHandler);
   assert ((logfile_ptr = fopen("/tmp/gpslog.bin", "w")) >= 0);
   
   GPS();
@@ -176,4 +109,74 @@ int main(void)
 }
 
 
-/*add int handler for exiting loop */
+//split initialization function?
+void GPS() // this function will be added to .h file when it is created. 
+{
+  
+  gpsmm gps_rec("localhost", DEFAULT_GPSD_PORT);   //finds GPS, see what # the default port is
+  
+  if (gps_rec.stream(WATCH_ENABLE | WATCH_JSON) == NULL) // ENABLE turns off repording modes, JSON turns on JSON reporting data
+  {
+    std::cerr << "No GPSD running.\n";
+    
+  }
+
+  struct gps_data_t gpsd_data;  
+  struct gps_data_t *dataPtr = &gpsd_data;
+  double comp;
+  
+  for (;;)  // change back while forever
+  {
+    
+    while (((dataPtr= gps_rec.read()) == NULL) ||
+             (dataPtr->fix.mode < MODE_2D)) 
+    {
+
+        // Do nothing until fix
+        //cout<<"Stuck trying to fix"<<endl;
+    }
+
+    // log the gps binary data
+    assert((fwrite(dataPtr, sizeof(struct gps_data_t), 1, logfile_ptr)) == true);
+      
+      
+       
+    cout << "Time, lat, lon parsing" << endl;
+    timestamp_t ts { dataPtr->fix.time };
+    auto newLatitude  { dataPtr->fix.latitude };
+    auto newLongitude { dataPtr->fix.longitude };
+    cout << "Time, lat, lon parsed" << endl;
+      
+    // convert GPSD's timestamp_t into time_t
+    time_t seconds { (time_t)ts };
+    auto   tm = *std::localtime(&seconds);
+
+    cout << "Time convert" << endl;
+
+    std::ostringstream oss;
+    oss << std::put_time(&tm, "%d-%m-%Y %H:%M:%S");
+    auto time_str { oss.str() };
+      
+    cout << "Calling GPS comp" << endl;
+    comp = instance1.gpsComp(newLatitude, newLongitude);
+    cout<<"Distance: "<<comp<<endl;
+    if (comp >= 3.0) 
+    {     
+      instance1.saveGPSData((double)(newLatitude), (double)(newLongitude), time_str); //fix after comp works
+    }
+      
+    
+  }
+}
+
+
+/* Close sockets after a Ctrl-C interrupt */
+
+void signalHandler(int signum)
+{
+  cout<<"Interupt signal \""<<signum<<"\" recieved"<<endl;
+
+  //close things if need be
+  instance1.~LOCATION();
+  exit(signum);
+}
