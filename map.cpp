3963626dc1516afcd93bc9e326c36b0013f2c927
Alec <aleclee1996@gmail.com>
2019-04-16 10:18:00 -0700

    moved everything to a .hpp file

--- map.cpp
+++ map.cpp
@@ -9,81 +9,20 @@
 #include <iostream>
 #include <string>
 #include <csignal>  // for ^c handler
+#include "map.hpp"  //for the use of map
 
 using namespace std;  //needed for use of string
-
-struct NODE
-{
-  struct MONITOR_DATA
-  {
-    double latitude;
-    double longitude;
-    string monitorID;
-  };
-  MONITOR_DATA monitor;
-  NODE *next;
-};
-
-class MAP
-{
-  private:
-    NODE *head, *tail;
-  
-  public:
-  //constuctors, destructors, and functions
-  
-    MAP()
-    {
-      head = NULL;
-      tail = NULL;
-    }
-    
-    ~MAP()
-    {
-      NODE *n = this->head, *current = NULL;
-      
-      while(n)
-      {
-        current = n;
-        n = n->next;
-        free(current);
-      }
-      
-      head = tail = NULL;
-    }
-    
-    void addNode (double newLat, double newLong, string newID);
-    int checkID(string ID);
-    void printMap();
-};
 
 MAP serverMap;
 
 
-int main(int argc, char** argv)
+
+
+int main()
 {
   std::cout<<"I compile"<<std::endl;
 }
 
-void MAP::addNode (double newLat, double newLong, string newID)
-{
-  NODE *tmp = new NODE;
-  tmp->monitor.latitude = newLat;
-  tmp->monitor.longitude = newLong;
-  tmp->monitor.monitorID = newID;
-  tmp->next = NULL;
-  
-  if(head == NULL)
-  {
-    head = tmp;
-    tail = tmp;
-  }
-  else
-  {
-    tail->next = tmp;
-    tail = tail->next;
-  }
-}
 
 //destroying class and exiting after ^c
 void signalHandler(int signum)
