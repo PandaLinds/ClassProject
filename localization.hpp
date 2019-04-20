f429c2b72ff967d1276bfe80cd13fddb2c8193ba
Alec <aleclee1996@gmail.com>
2019-04-20 13:49:51 -0700

    make so matches .cpp

--- localization.hpp
+++ localization.hpp
@@ -12,7 +12,7 @@
 
 #include <libgpsmm.h> // for GPS
 
-using namespace std;
+using namespace std; //won't compile without this
 
 //add class to .h file when created to match style guide.
 class LOCATION
@@ -72,65 +72,5 @@ double LOCATION::gpsComp(double lat, double lon)
 }
 
 
-//split initialization function?
-void GPS() // this function will be added to .h file when it is created. 
-{
-  
-  gpsmm gps_rec("localhost", DEFAULT_GPSD_PORT);   //finds GPS, see what # the default port is
-  
-  if (gps_rec.stream(WATCH_ENABLE | WATCH_JSON) == NULL) // ENABLE turns off repording modes, JSON turns on JSON reporting data
-  {
-    std::cerr << "No GPSD running.\n";
-    
-  }
 
-  struct gps_data_t gpsd_data;  
-  struct gps_data_t *dataPtr = &gpsd_data;
-  double comp;
-  
-  for (;;)  // change back while forever
-  {
-    
-    while (((dataPtr= gps_rec.read()) == NULL) ||
-             (dataPtr->fix.mode < MODE_2D)) 
-    {
-
-        // Do nothing until fix
-        //cout<<"Stuck trying to fix"<<endl;
-    }
-
-    // log the gps binary data
-  //  assert((fwrite(dataPtr, sizeof(struct gps_data_t), 1, logfile_ptr)) == true);
-      
-      
-       
-    cout << "Time, lat, lon parsing" << endl;
-    timestamp_t ts { dataPtr->fix.time };
-    auto newLatitude  { dataPtr->fix.latitude };
-    auto newLongitude { dataPtr->fix.longitude };
-    cout << "Time, lat, lon parsed" << endl;
-      
-    // convert GPSD's timestamp_t into time_t
-    time_t seconds { (time_t)ts };
-    auto   tm = *std::localtime(&seconds);
-
-    cout << "Time convert" << endl;
-
-    std::ostringstream oss;
-    oss << std::put_time(&tm, "%d-%m-%Y %H:%M:%S");
-    auto time_str { oss.str() };
-      
-//    cout << "Calling GPS comp" << endl;
-//    comp = instance1.gpsComp(newLatitude, newLongitude);
-//    cout<<"Distance: "<<comp<<endl;
-//    if (comp >= 3.0) 
-//    {     
-//      instance1.saveGPSData((double)(newLatitude), (double)(newLongitude), time_str); //fix after comp works
-//    }
-
-// save to file to compare first?      
-    
-  }
-}
-
-
+void GPS(); //to be defined by user
