1200bc479c9660f84e398c9c36449c4e66699231
Alec <aleclee1996@gmail.com>
2019-04-29 16:54:59 -0700

    acoustic testing is working

--- acousticEm.cpp
+++ acousticEm.cpp
@@ -1,10 +1,13 @@
-#inlcude "acousticEm.hpp"
+#include "acousticEm.hpp"
+#include <iostream>
 
 void emulateDetection() 
 {
   //copied from SERVER code
-  penlog("R-PI-MONITOR", LOG_CONS | LOG_PID, LOG_USER); /* Log to monitor.txt */ 
-  system("cat /var/log/syslog | grep R-PI-MONITOR > monitorLog.txt"); 
-  syslog(ACOUSTIC_MONITOR, "%s", "Starting log...\n");  
-  syslog(ACOUSTIC_MONITOR, "%s", "I think I heard a drone.\n");
+  //openlog("R-PI-MONITOR", LOG_CONS | LOG_PID, LOG_USER); /* Log to monitor.txt */ 
+  //system("cat /var/log/syslog | grep R-PI-MONITOR > monitorLog.txt"); 
+  //syslog(LOG_NOTICE, "%s", "Starting log...\n");  
+  //syslog(LOG_NOTICE, "%s", "Initialization successful, beginning to serve clients...\n");
+  fprintf(acousticFilePtr, "\n");
+  //fclose(acousticFilePtr); //used in test code for now
 }
