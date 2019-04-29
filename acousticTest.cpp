5bb04bcfea3ff95737482f74c2dee1e0810462bd
Alec <aleclee1996@gmail.com>
2019-04-29 16:56:16 -0700

    testing works

--- acousticTest.cpp
+++ acousticTest.cpp
@@ -1,7 +1,36 @@
+//random example code from:
+// https://stackoverflow.com/questions/18726102/what-difference-between-rand-and-random-functions
+
+#include "acousticEm.hpp" //to test acoustic
 #include <iostream>
-#include "acousticEm.hpp"
+#include <random> // for random number generator
+#include <csignal>  // for ^c handler
+
+#define FOREVER (1)
+
+void signalHandler(int signum);
 
 int main()
 {
-  cout<<"I complile"<<endl;
+  signal(SIGINT, signalHandler);
+  std::random_device r;
+  std::seed_seq seed{r(), r(), r(), r(), r(), r(), r(), r()};
+  std::mt19937 eng{seed};
+  
+  std::uniform_int_distribution<> dist(1,100);
+  
+  while(FOREVER)
+  {
+    if((dist(eng))<50)
+    {
+      emulateDetection();
+    }
+  }
+}
+
+void signalHandler(int signum)
+{
+  std::cout<<"Interupt signal \""<<signum<<"\" recieved"<<std::endl;
+  exit(signum);
+  fclose(acousticFilePtr);
 }
