29bdd5e765469f20c3e69e629794d1c51980fb43
Alec Lindsey <aleclee1996@gmail.com>
2019-04-29 21:17:19 -0700

    got all my files to work on some file

--- monitor.cpp
+++ monitor.cpp
@@ -2,8 +2,11 @@
 #include <string>
 #include <csignal>  // for ^c handler
 #include <pthread.h>
+#include <random>
+#include "acousticEm.hpp"
 #include "map.hpp"  //for the use of map
 #include "localization.hpp"
+
 
 using namespace std;  //needed for use of string
 
@@ -18,27 +21,51 @@ void* GPSthread(void*)
   trackGPS();
 }
 
+void* acousticthread(void*)
+{
+  signal(SIGINT, signalHandler);
+  std::random_device r;
+  std::seed_seq seed{r(), r(), r(), r(), r(), r(), r(), r()};
+  std::mt19937 eng{seed};
+  
+  std::uniform_int_distribution<> dist(1,100);
+  
+  while(1)
+  {
+    if(((dist(eng))==1)&&((dist(eng))==1)&&((dist(eng))==1))
+    {
+      emulateDetection();
+    }
+  }
+}
+
 int main()
 {
 	
   double testLat, testLong;
   string testID;
-  pthread_t GPS;
+  pthread_t GPS, acoustic;
   MAP testMap;
   //registering signal SIGINT and signal handler 
   signal(SIGINT, signalHandler);
   
-  pthread_create(&GPS, NULL, &GPSthread, NULL);
   
-//  for (int i = 0; i<5; i++)
-//  {
-//    cout<<"enter a latitude: "; cin>>testLat;
-//    cout<<"enter a longitude: "; cin>>testLong;
-//    cout<<"enter an ID: "; cin>>testID;
-//    testMap.addNode(testLat, testLong, testID);
-//    testMap.printMap();
-//  }
+  pthread_create(&GPS, NULL, &GPSthread, NULL);
+  pthread_create(&GPS, NULL, &acousticthread, NULL);
+  
+  for (int i = 0; i<5; i++)
+  {
+    cout<<"enter a latitude: "; cin>>testLat;
+    cout<<"enter a longitude: "; cin>>testLong;
+    cout<<"enter an ID: "; cin>>testID;
+    testMap.addNode(testLat, testLong, testID);
+    testMap.printMap();
+  }
   pthread_join(GPS, NULL);
+  pthread_join(acoustic,NULL);
+  fclose(locationFilePtr);
+  fclose(acousticFilePtr);
+  fclose(mapFilePtr);
 }
 
 void updateMap ()
@@ -50,5 +77,8 @@ void updateMap ()
 void signalHandler(int signum)
 {
   cout<<"Interupt signal \""<<signum<<"\" recieved"<<endl;
+  fclose(locationFilePtr);
+  fclose(acousticFilePtr);
+  fclose(mapFilePtr);
   exit(signum);
 }
