20738547d105b1e2f853e429db5943cfb3ad0d89
Alec Lindsey <aleclee1996@gmail.com>
2019-04-29 21:17:58 -0700

    added logging

--- acousticEm.hpp
+++ acousticEm.hpp
@@ -3,7 +3,10 @@
 
 #include <iostream>
 #include <syslog.h>
+
 static FILE *acousticFilePtr= fopen("monitorLog.txt", "a");
+
+//function not able to be reached by monitor
 void emulateDetection();
 
 #endif
