ebea8b4c24dcbe1e210e3afb74b0f274c408b3fd
Alec <aleclee1996@gmail.com>
2019-07-11 09:29:26 -0700

    trying to add all moitor components

--- monitor.cpp
+++ monitor.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <string>
 #include <csignal>  // for ^c handler
-#include <pthread.h>
 #include <random>
-#include "acousticEm.hpp"
-#include "map.hpp"  //for the use of map
+#include "capture.hpp"
 #include "localization.hpp"
+#include <pthread.h>
+#include <unistd.h>
+
 
 
 using namespace std;  //needed for use of string
@@ -13,59 +14,36 @@ using namespace std;  //needed for use of string
 void signalHandler(int signum);
 void updateMap();
 
-
-
-
 void* GPSthread(void*)
 {
   trackGPS();
 }
 
-void* acousticthread(void*)
+void* motionthread(void*)
 {
-  signal(SIGINT, signalHandler);
-  std::random_device r;
-  std::seed_seq seed{r(), r(), r(), r(), r(), r(), r(), r()};
-  std::mt19937 eng{seed};
-  
-  std::uniform_int_distribution<> dist(1,100);
-  
-  while(1)
-  {
-    if(((dist(eng))==1)&&((dist(eng))==1)&&((dist(eng))==1))
-    {
-      emulateDetection();
-    }
-  }
+  detect();
 }
 
 int main()
 {
-	
-  double testLat, testLong;
+	double testLat, testLong;
   string testID;
-  pthread_t GPS, acoustic;
-  MAP testMap;
+  pthread_t GPS, motion;
   //registering signal SIGINT and signal handler 
   signal(SIGINT, signalHandler);
   
   
   pthread_create(&GPS, NULL, &GPSthread, NULL);
-  pthread_create(&GPS, NULL, &acousticthread, NULL);
+  pthread_create(&GPS, NULL, &motionthread, NULL);
   
-  for (int i = 0; i<5; i++)
-  {
-    cout<<"enter a latitude: "; cin>>testLat;
-    cout<<"enter a longitude: "; cin>>testLong;
-    cout<<"enter an ID: "; cin>>testID;
-    testMap.addNode(testLat, testLong, testID);
-    testMap.printMap();
-  }
   pthread_join(GPS, NULL);
-  pthread_join(acoustic,NULL);
+  pthread_join(motion,NULL);
   fclose(locationFilePtr);
-  fclose(acousticFilePtr);
-  fclose(mapFilePtr);
+  fclose(motionFilePtr);
+  
+  
+  
+  
 }
 
 void updateMap ()
@@ -78,7 +56,6 @@ void signalHandler(int signum)
 {
   cout<<"Interupt signal \""<<signum<<"\" recieved"<<endl;
   fclose(locationFilePtr);
-  fclose(acousticFilePtr);
-  fclose(mapFilePtr);
+  fclose(motionFilePtr);
   exit(signum);
 }
