22fe7f7b391af52af91eeb6f5bc213b6a0273512
Alec <aleclee1996@gmail.com>
2019-07-14 21:49:19 -0700

    linking modules together

--- capture.hpp
+++ capture.hpp
@@ -7,7 +7,20 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
-int detect();
+using namespace cv;
+
+//#define HRES 640
+//#define VRES 480
+
+#define DHRES 640
+#define DVRES 480
+
+#define ESC_KEY 27
+
+
+
+int MotionDetect();
+
 
 static FILE *motionFilePtr= fopen("monitorLog.txt", "a");    //log file
 static FILE *motionDataPtr= fopen("motionData.txt", "a");  //save data
